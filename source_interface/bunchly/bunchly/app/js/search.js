222325a06c4f722ef26a3a1fb04eed7ec2cdf068
arnaud <arnauz94>
2019-01-20 00:09:39 +0100

    temp

--- source_interface/bunchly/bunchly/app/js/search.js
+++ source_interface/bunchly/bunchly/app/js/search.js
@@ -3,23 +3,10 @@ var doneTypingInterval = 100;
 var $input = $('#search-input');
 var button = document.getElementById("search-button");
 $(document).ready(function(){
-    $("#search-input").keydown(function(e){
-        if(e.originalEvent.key.length == 1 || e.which == 8) 
-        {
-            clearTimeout(typingTimer);
-            typingTimer = setTimeout(doneTyping, doneTypingInterval);
-        }
+    $("#search-button").click(function(e){
+        
     });
-    $("#search-input").keyup(function(e){
-        if(e.originalEvent.key.length == 1 || e.which == 8)
-        {
-            clearTimeout(typingTimer);
-        }
-    });
-    function doneTyping () {
-        console.log($("#search-input").val());
-    }
-
+    
     
 
 });
